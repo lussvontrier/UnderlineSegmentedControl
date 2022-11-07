3ea9c35bd699486502f0b37979c5279e2a45e359
Lusine <lusine@getsteven.com>
2022-11-07 21:52:30 +0400

    moved podspec

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Lusine Magauzyan on 07.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
